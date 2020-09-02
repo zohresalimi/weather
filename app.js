93b165b91f8ca1d06897b1a57113bddfa55e60fb
zohresalimi <zohre.1987@gmail.com>
2020-09-02 16:21:31 +0200

    change addHeader function

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
 
 document.addEventListener("DOMContentLoaded", function(){
-    const weather = [];
+    const weatherList = [];
     
     document.querySelector('#add').addEventListener('click', (e) => {
         e.preventDefault();
         const temperature = document.querySelector('#temperature').value;
         const date = document.querySelector('#date').value;
-        if(!temperature && !date) {
-            e.target.disabled = true;
+        if(!temperature || !date) {
+            return false
         }
         const newObj = {temperature, date}
-        weather.push(newObj)
-        addHeader(newObj)
+        weatherList.push(newObj)
+        addHeader()
         addRow(newObj)
         document.querySelector('#temperature').value=''
         document.querySelector('#date').value=''
     })
 });
 
-const addHeader = (newObj) =>{
-    const header = Object.keys(newObj);
+const addHeader = () =>{
+    let inputs = document.querySelectorAll('input.form__input');
     var recordTable = document.querySelector('table')
     if(!recordTable.tHead){
         var tHead = recordTable.createTHead();
         var tr = tHead.insertRow(-1);
-        for(let i=0; i < header.length; i++) {
+        inputs.forEach((input) =>{
             var th = document.createElement('th');
-            th.innerHTML= header[i]
+            th.innerHTML= input.getAttribute('name')
             tr.appendChild(th)
-        }
+        })
     }
-
 }
 
 const addRow = (data) =>{
     const table = document.querySelector('table')
-    const tbody = document.querySelector('tbody')
+    const tbody = document.createElement('tbody')
     const tr = document.createElement('tr')
     const header = Object.keys(data);
 
     for(let j=0; j<header.length; j++){
-        var cell = document.createElement('td')
-        cell.innerHTML = data[header[j]]
-        tr.appendChild(cell)
+        var td = document.createElement('td')
+        td.innerHTML = data[header[j]]
+        tr.appendChild(td)
     }
     tbody.appendChild(tr)
     table.appendChild(tbody)
